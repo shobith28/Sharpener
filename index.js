e20341475658740fbee091300dce0c2625b1e121
shobith28 <shobith8005@gmail.com>
2024-08-08 20:50:52 +0530

    added js and css

---
+++ index.js
@@ -0,0 +1,13 @@
+const mainHeading=document.getElementById('main-heading');
+mainHeading.textContent='Fruit World'
+mainHeading.style.color='orange'
+
+const header=document.getElementById('header');
+header.style.backgroundColor='green'
+header.style.borderBottom='5px solid orange'
+
+const basketHeading=document.getElementById('basket-heading')
+basketHeading.style.color='green'
+
+const thanks=document.getElementById('thanks')
+thanks.innerHTML='<P>Please visit us again</P>'
